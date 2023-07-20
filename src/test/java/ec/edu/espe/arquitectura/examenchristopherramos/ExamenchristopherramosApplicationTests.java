ca60ab6b44e71e5b9601c2a73aa70b1b84c44a8d
Christopher <cramos@plugthem.social>
2023-07-19 19:13:32 -0500

    First Project

---
+++ src/test/java/ec/edu/espe/arquitectura/examenchristopherramos/ExamenchristopherramosApplicationTests.java
@@ -0,0 +1,13 @@
+package ec.edu.espe.arquitectura.examenchristopherramos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamenchristopherramosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
